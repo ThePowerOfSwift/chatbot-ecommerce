5d0dc8c11cfa9bce0c9628a864d88258b4ee57f6
Elizabeth Nathania W <liza_1805@hotmail.com>
2018-12-17 23:12:12 +0900

    add templates

---
+++ templates/plain/search.php
@@ -0,0 +1,8 @@
+<?php
+    $contents = "This is the information of your product:\n";
+    $contents .= "Product name: " . $data['product_name'] ."\n";
+    $contents .= "Product price: " . $data['product_price'] . "\n";
+    $contents .= "Product seller: " . $data['product_seller'] . "\n";
+    $contents .= "Product description: " . $data['product_desc'] . "\n";
+    $contents .= "Product link: " . $data['product_link'] . "\n";
+?>
---
+++ templates/plain/start.php
@@ -0,0 +1,6 @@
+<?php
+    $contents = "Welcome to Gmarket Bot. This bot can help you to find information about your stuff before you buy it on gmarket.
+    Enter your stuff to start searching.
+    
+    Thank you."
+?>
