2d60bc4d2e10ee24572077fa840905ceac50b8c5
Elizabeth Nathania W <liza_1805@hotmail.com>
2018-12-18 14:54:20 +0900

    update start.php

--- templates/plain/start.php
+++ templates/plain/start.php
@@ -1,6 +1,6 @@
 <?php
-    $contents = "Welcome to Gmarket Bot. This bot can help you to find information about your stuff before you buy it on gmarket.
-    Enter your stuff to start searching.
+    $contents = "Welcome to Coupang Bot. This bot can help you to find information about your stuff before you buy it on Coupang.
+    Enter the name of the product to start searching.
     
     Thank you."
 ?>
