c8ca7f5a4b4fa12b2ca60a5d926d1f1058b67c5a
Elizabeth Nathania W <liza_1805@hotmail.com>
2018-12-18 15:05:29 +0900

    update template start

--- templates/plain/start.php
+++ templates/plain/start.php
@@ -1,5 +1,5 @@
 <?php
-    $contents = "Welcome to Coupang Bot. This bot can help you to find information about your stuff before you buy it on Coupang.
+    $contents = "Hello " . $data . "!.\nWelcome to Coupang Bot. This bot can help you to find information about your stuff before you buy it on Coupang.
     Enter the name of the product to start searching.
     
     Thank you."
