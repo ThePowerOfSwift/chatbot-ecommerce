afb998718ed708ed76369b35d47c841f03fc0fbd
Elizabeth Nathania W <liza_1805@hotmail.com>
2018-12-18 13:49:06 +0900

    update main.php

--- main.php
+++ main.php
@@ -1,4 +1,7 @@
 <?php
+require_once dirname(__FILE__) . '/Controller.php';
+
+$telegramBot = new BotTelegram(BOT_TOKEN);
 
 class BotTelegram {
     public $bot_token;
@@ -8,6 +11,8 @@ class BotTelegram {
 
         $input = file_get_contents("php://input");
         $req = json_decode($input, true);
+		
+		file_put_contents('request.txt', print_r($req,true));
 
         if(isset($req['message'])) {
             $content = $req['message'];
@@ -15,6 +20,7 @@ class BotTelegram {
             $this->controller = new Controller();
             $res = $this->controller->Parsing($req);
             $output = $this->controller->sendMessages($req, $res);
+			file_put_contents('respons.txt', print_r($output,true));
         }
     }
 }
